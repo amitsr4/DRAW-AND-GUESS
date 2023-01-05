69c756c6d439645d9f309f47402355d488fc494c
amit <amitsr4@gmail.com>
2023-01-06 01:19:06 +0200

    need to set the canvas

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs'); 
 
@@ -10,9 +11,37 @@ const players = [
     {id: 2, name: 'player 2'},    
 ];
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//middleware & static files
+app.use(express.static('public'));
+
+// app.get('/word-choosing-view.ejs', (req,res)=>{
+//     res.render('word-choosing-view', {title: 'Home'});
+// });
+
 app.get('/', (req,res)=>{
-    console.log("Server Up");
-    app.render('welcome-view');
+    res.render('welcome-view', {title: 'Home'});
+});
+
+app.post('/drawing-view',(req,res)=>{
+    const word =req.body.word;
+    res.render('drawing-view', {title: 'Drawing', word})
+});
+
+// app.post('/drawing-view', (req,res)=>{
+//     const draw = new Draw(req.body);
+
+//     draw.save().then((result) =>{
+//         res.redirect('/waiting-view')
+//     })
+//     .cath((err)=>{ 
+//         console.log(err);
+//     })
+// });
+
+app.get('/word-choosing-view', (req,res)=>{
+    res.render('word-choosing-view', {title: 'Choose word'});
 });
 
 app.listen(PORT, () => {
