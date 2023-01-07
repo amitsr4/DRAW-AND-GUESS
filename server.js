32a483994f648772d30fca9db91568e7006fc83e
amit <amitsr4@gmail.com>
2023-01-07 17:16:01 +0200

    another commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const { identical } = require('ramda');
 
 app.set('view engine', 'ejs'); 
 
