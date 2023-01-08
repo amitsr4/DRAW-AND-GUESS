94954e93fdd0c0306939d168c5506b5eb9e8e918
amit <amitsr4@gmail.com>
2023-01-08 15:47:33 +0200

    before changing the server by id

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let currWord;
 let currDrawer ='free';
 let currGuesser= 'free';
 let avialability = 'available';
-
+let draw;
 
 
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true}));
 
 //middleware & static files
 app.use(express.static('public'));
-app.use(express.json()); //TODO not sure i need it.
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     if(currDrawer != 'free' && currDrawer != 'free'){
@@ -25,24 +25,38 @@ app.get('/', (req,res)=>{
     }
     res.render('welcome-view', {title: 'Home'});
 });
+app.get('/waiting-view', (req,res)=>{
+    res.render('waiting-view', {title: 'Waiting'});
+});
+
+app.post('/waiting-view',(req,res)=>{
+    if(currGuesser){
+    draw = req.body.imgBase64;
+    currWord = req.body.word;
+    res.render('guessing-view', {title: 'Waiting', draw});
+    }
+    else{
+        res.render('waiting-view', {title: 'Waiting'});
+    }
+});
 
 app.post('/word-choosing-view',(req,res)=>{
     const role = req.body['button']
     const name = req.body['name']
     if(role=='Drawer'){ 
-        if(currDrawer != 'free'){
-            console.log("The drawer role is taken")
-        }else {
+        // if(currDrawer != 'free'){
+        //     console.log("The drawer role is taken")
+        // }else {
             currDrawer = name;
-            res.render('word-choosing-view', {title: 'Choosing', role, name})
-        }
+            res.render('word-choosing-view', {title: 'Choosing', role, name});
+        // }
     } else{
-        if(currGuesser != 'free'){
-            if(currDrawer!= 'free') console.log("Please wait until one player quit")
-            else console.log("The Guesser role is taken")
-        }else {currGuesser = name;
-        res.render('guessing-view', {title: 'Guessing', role, name})
-        }
+        // if(currGuesser != 'free'){
+        //     if(currDrawer!= 'free') console.log("Please wait until one player quit")
+        //     else console.log("The Guesser role is taken")
+        // }else {currGuesser = name;
+        res.render('guessing-view', {title: 'Guessing', role, name});
+        // }
     } 
    
 });
@@ -67,9 +81,8 @@ app.get('/guessing-view',(req,res)=>{
 
 app.post('/drawing-view',(req,res)=>{
     currWord = req.body.word;
-    res.render('drawing-view', {title: 'Drawing', currWord})
+    res.render('drawing-view', {title: 'Drawing', currWord});
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server started, listening on port: http://localhost:${PORT}`)
